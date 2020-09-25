bc4045d209bd598fbde5ea5160ad7f240ecea770
enricllagostera <enricllagostera@gmail.com>
2020-09-24 20:47:18 -0400

    line completed visual effect, better rendering logic

--- Matrix.gd
+++ Matrix.gd
@@ -3,12 +3,12 @@ extends Node2D
 
 export var tick_interval = 0.5
 var state = Dictionary()
-var size = Vector2(10,45)
+var size = Vector2(10,44)
 var tetro_prefab
 var pieces = []
 var current_player = Game.Player.RIGHT
 var lines = 0
-var spawn_points = [Vector2(5,1), Vector2(5,43)]
+var spawn_points = [Vector2(5,1), Vector2(5,42)]
 var debug_block_prefab
 var debug_blocks = Dictionary()
 var is_updating = false
@@ -40,14 +40,17 @@ func debug_render():
 	pass
 
 
-func is_block_in_piece(block, piece):
-	for c in piece.rel_blocks:
-		if c+piece.coord == block:
+func is_colliding_blocks(target_blocks, self_blocks = [], other_piece = null):
+	for tb in target_blocks:
+		if state[tb] == 1:
+			return true
+		if other_piece != null and other_piece.get_abs_blocks().has(tb):
 			return true
 	return false
+	pass
 
 
-func is_colliding(self_blocks, base_coord, blocks_to_check, check_self = true):
+func is_colliding_scenario(self_blocks, base_coord, blocks_to_check, check_self = true):
 	var tetro = []
 	for b in self_blocks:
 		tetro.append(b+base_coord)
@@ -76,39 +79,37 @@ func rotate_tetro(tetro, is_clockwise):
 	var target_blocks_abs = Game.calc_tetro_abs_coords(tetro.coord, tetro.piece, rot_table[tetro.rot])
 	if is_out_of_bounds(target_blocks_abs):
 		return
-	if is_colliding(tetro.rel_blocks, tetro.coord, target_blocks_abs):
+	if is_colliding_blocks(target_blocks_abs, tetro.get_abs_blocks()):
 		return
-	for b in tetro.rel_blocks:
-		state[tetro.coord+b] = 0
 	tetro.rel_blocks = Game.calc_tetro_rel_coords(tetro.piece, rot_table[tetro.rot])
-	for b in tetro.rel_blocks:
-		state[tetro.coord+b] = 1
 	tetro.rot = rot_table[tetro.rot]
-	debug_render()
 	pass
 
 
 func shift_tetro(tetro, dir, player):
-	var target_blocks = [] 
+	var target_blocks = tetro.get_abs_blocks() 
 	for b in tetro.rel_blocks:
 		target_blocks.append(tetro.coord+b+Game.SHIFTS[dir])
 	if is_out_of_bounds(target_blocks):
 		return
-	if is_colliding(tetro.rel_blocks, tetro.coord, target_blocks):
-#		just started touching the bottom of the piece
-		if (player == Game.Player.LEFT and dir == Game.Dir.D) or (player == Game.Player.RIGHT and dir == Game.Dir.U):
-			if not tetro.locking:
-				tetro.lockdown_started()
+	if is_colliding_blocks(target_blocks, tetro.get_abs_blocks(), pieces[Game.Player.LEFT if player == Game.Player.RIGHT else Game.Player.RIGHT]):
+		if not tetro.locking:
+			tetro.lockdown_started()
 		return
 	if tetro.locking:
 		tetro.lockdown_cancelled()
-#	apply movement
-	for b in tetro.rel_blocks:
-		state[tetro.coord+b] = 0
-	tetro.coord += Game.SHIFTS[dir]
-	for b in tetro.rel_blocks:
-		state[tetro.coord+b] = 1
+#
+##	just started touching the bottom of the piece
+#		if (player == Game.Player.LEFT and dir == Game.Dir.D) or (player == Game.Player.RIGHT and dir == Game.Dir.U):
+#			if not tetro.locking:
+#				tetro.lockdown_started()
+	move_tetro(tetro, dir)
 	debug_render()
+	pass
+
+
+func move_tetro(tetro, dir):
+	tetro.coord += Game.SHIFTS[dir]
 	pass
 
 
@@ -120,25 +121,23 @@ func spawn(origin, player, piece):
 	new_tetro.rel_blocks = Game.calc_tetro_rel_coords(new_tetro.piece, new_tetro.rot)
 	new_tetro.player = player
 	var target_blocks = Game.calc_tetro_abs_coords(new_tetro.coord, new_tetro.piece, new_tetro.rot)
-	if is_colliding(new_tetro.rel_blocks, new_tetro.coord, target_blocks, false):
+	if is_colliding_blocks(target_blocks):
 		print("game over")
 		return 
-	add_child(new_tetro)
-	for b in new_tetro.rel_blocks:
-		state[origin+b] = 1
-	if pieces[player] is Node:
-		pieces[player].queue_free()
+	$Grid.add_child(new_tetro)
 	pieces[player] = new_tetro
 	pass
 
 
 func block_locked(player):
 	is_updating = true
+	for b in pieces[player].get_abs_blocks():
+		state[b] = 1
+	pieces[player].queue_free()
 	update_score()
 	spawn(spawn_points[player], player, Game.Piece.J)
 	debug_render()
 	is_updating = false
-	
 
 
 func _ready():
@@ -150,12 +149,11 @@ func _ready():
 		for col in range(0,10):
 			state[Vector2(col, row)] = 0
 			debug_blocks[Vector2(col, row)] = debug_block_prefab.instance()
-			$DebugGrid.add_child(debug_blocks[Vector2(col, row)])
+			$Grid.add_child(debug_blocks[Vector2(col, row)])
 			debug_blocks[Vector2(col, row)].position = Vector2(col, row) * 16
 			debug_blocks[Vector2(col, row)].get_node("Solid").visible = false
 	spawn(spawn_points[Game.Player.LEFT], Game.Player.LEFT, Game.Piece.J)
 	spawn(spawn_points[Game.Player.RIGHT], Game.Player.RIGHT, Game.Piece.J)
-	debug_render()
 	pass
 
 
@@ -165,25 +163,26 @@ func update_score():
 	var rows = get_completed_rows()
 	if rows.size() == 0:
 		return
+	$Tween.interpolate_property($Grid, "position", Vector2(-352,88 + rows.size()*8), Vector2(-352,88), .3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	$Tween.start()
 	for r in rows:
 		lines += 1
-		if r < 23:
+		if r < size.y / 2:
 			for s in range(r, 1, -1):
 				for c in range(0, size.x):
-					if is_block_in_piece(Vector2(c,s), pieces[Game.Player.LEFT]) or is_block_in_piece(Vector2(c,s), pieces[Game.Player.RIGHT]):
-						continue
 					state[Vector2(c,s)] = state[Vector2(c,s-1)] if s>0 else 0
 		else: 
 			for s in range(r, size.y-1, 1):
 				for c in range(0, size.x):
-					if is_block_in_piece(Vector2(c,s), pieces[Game.Player.LEFT]) or is_block_in_piece(Vector2(c,s), pieces[Game.Player.RIGHT]):
-						continue
 					state[Vector2(c,s)] = state[Vector2(c,s+1)] if s<size.y else 0
 	print("lines: ", lines)
 	pass
 	
 
 func _process(delta):
+	pieces[0].render()
+	pieces[1].render()
+	
 	if is_updating:
 		return
 	if Input.is_action_just_released("pl_rot_c"):
@@ -207,6 +206,8 @@ func _process(delta):
 
 
 func _on_TickTimer_timeout():
+	if is_updating:
+		return
 	for i in range(0,2):
 		shift_tetro(pieces[i], Game.Dir.D if i == Game.Player.LEFT else Game.Dir.U, i)
 	pass
--- Tetro.gd
+++ Tetro.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var rel_blocks = [Vector2(0,0), Vector2(-1,-1), Vector2(-1,0), Vector2(1,0)]
-var visual_offset = Vector2()
+onready var visual_blocks = [$Block0, $Block1, $Block2, $Block3]
 var offset = Vector2()
 export var coord = Vector2()
 var rot = Game.Rot.R0
@@ -10,38 +10,32 @@ var locking = false
 var locked = false
 var player = -1
 
-var visual_offset_table = {
-	Game.Piece.J: Vector2(-1.5, -1.5)
-}
-
-
-func _ready():
-	position = coord * Game.FTILE
-	$Visual.offset = visual_offset_table[Game.Piece.J] * Game.FTILE
-	pass # Replace with function body.
-
 
 func rotate_clockwise():
 	var target_rot = Game.ROT_CLOCKWISE[rot]
-	print(target_rot)
 	rot = target_rot
-	render_rotation()
 
-func render_rotation():
-	if rot == Game.Rot.R0:
-		$Visual.rotation = deg2rad(0)
-	if rot == Game.Rot.RR:
-		$Visual.rotation = deg2rad(90)
-	if rot == Game.Rot.R2:
-		$Visual.rotation = deg2rad(180)
-	if rot == Game.Rot.RL:
-		$Visual.rotation = deg2rad(270)
+
+func render():
+	var blocks = get_abs_blocks()
+	for i in range(0, blocks.size()):
+		visual_blocks[i].position = blocks[i] * Game.FTILE
+	pass
+
+
+func get_abs_blocks():
+	var abs_blocks = []
+	for b in rel_blocks:
+		abs_blocks.append(b+coord)
+	return abs_blocks
 
 
 func lockdown_cancelled():
 	print("tetro lock cancelled")
 	locking = false
 	locked = false
+	for vb in visual_blocks:
+		vb.modulate = Color.white
 	$LockDownTimer.stop()
 	pass
 
@@ -52,7 +46,7 @@ func lockdown_completed():
 	locked = true
 	$LockDownTimer.stop()
 #	var pre = OS.get_ticks_msec()
-	get_parent().block_locked(player)
+	get_parent().get_parent().block_locked(player)
 #	print("ms: ", OS.get_ticks_msec() - pre)
 	pass
 
@@ -60,6 +54,8 @@ func lockdown_completed():
 func lockdown_started():
 	print("tetro locking")
 	locking = true
+	for vb in visual_blocks:
+		vb.modulate = Color.red
 	$LockDownTimer.start()
 	pass
 
